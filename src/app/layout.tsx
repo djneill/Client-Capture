3d4fd4dd6eee8269fce474d76ea3d6fb60712a15
DJ Neill <dj.neill856@gmail.com>
2024-01-22 23:58:59 -0600

    fix bug breaking dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { ThemeProvider } from "../components/providers/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
