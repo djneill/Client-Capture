0fe8480f1ff86998b148cd4a7695990f109d3498
DJ Neill <dj.neill856@gmail.com>
2024-01-24 04:40:49 -0600

    start admin / dashboard layout

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useTheme } from 'next-themes'
+import { Tabs, TabsList, TabsTrigger } from './ui/tabs'
+import { useEffect, useState } from 'react'
+import { SunIcon } from '@radix-ui/react-icons'
+
+function ThemeSwitcher() {
+    const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) return null
+    return (
+        <Tabs defaultValue={theme}>
+            <TabsList className='border'>
+                <TabsTrigger
+                    value='light'
+                    onClick={() => setTheme('light')}
+                >
+                    <SunIcon className='h-[1.2rem] w-[1.2rem]' />
+                </TabsTrigger>
+            </TabsList>
+        </Tabs>
+    )
+}
+
+export default ThemeSwitcher
