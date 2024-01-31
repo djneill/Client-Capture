473fed644827e52d7a4069b6a9649ade6118f457
DJ Neill <dj.neill856@gmail.com>
2024-01-31 04:31:11 -0600

    continue analytics cards

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
 import { GetFormStats } from "../../../../actions/form";
-import { LuView } from "react-icons/lu";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Separator } from "@/components/ui/separator";
 import { Suspense } from "react";
+import { LuView } from "react-icons/lu";
+import { FaWpforms } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
+import { TbArrowBounce } from "react-icons/tb";
 
-export default function Page() {
+export default function Dashboard() {
   return (
     <div className='container pt-4'>
       <Suspense fallback={<StatsCards loading={true} />}>
         <CardStatsWrapper />
       </Suspense>
+      <Separator className='my-6' />
+      <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
+      <Separator className='my-6' />
     </div>
   );
 }
@@ -36,6 +43,33 @@ function StatsCards(props: StatsCardProps) {
       loading={loading}
       className='shadow-md shadow-blue-600'
     />
+
+    <StatsCard
+      title='Total submissions'
+      icon={<FaWpforms className='text-yellow-600' />}
+      helperText='Total number of form submissions'
+      value={data?.visits.toLocaleString() || ''}
+      loading={loading}
+      className='shadow-md shadow-yellow-600'
+    />
+
+    <StatsCard
+      title='Submission rate'
+      icon={<HiCursorClick className='text-green-600' />}
+      helperText='Total number of form submissions'
+      value={data?.visits.toLocaleString() + '%' || ''}
+      loading={loading}
+      className='shadow-md shadow-green-600'
+    />
+
+    <StatsCard
+      title='Bounce rate'
+      icon={<TbArrowBounce className='text-red-600' />}
+      helperText='Form bounce rate'
+      value={data?.visits.toLocaleString() + '%' || ''}
+      loading={loading}
+      className='shadow-md shadow-red-600'
+    />
   </div>
 }
 
@@ -49,17 +83,19 @@ function StatsCard({ title, value, icon, helperText, loading, className, }: {
 }) {
   return (
     <Card className={className}>
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle className="text-sm font-medium text-muted-foreground">{title}</CardTitle>
         {icon}
       </CardHeader>
       <CardContent>
         <div className='text-2xl font-bold'>
           {loading && (
             <Skeleton>
-              <span>0</span>
+              <span className="opacity-0">0</span>
             </Skeleton>)}
+          {!loading && value}
         </div>
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
       </CardContent>
     </Card>
   )
